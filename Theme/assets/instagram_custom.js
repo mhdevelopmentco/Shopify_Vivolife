5fed53c6fbe2d4def9d26a282ed04dc6c6a37f5b
mhdevelopmentco <85754399+mhdevelopmentco@users.noreply.github.com>
2021-06-17 06:07:11 +0800

    Update instagram_custom.js

--- Theme/assets/instagram_custom.js
+++ Theme/assets/instagram_custom.js
@@ -7,7 +7,7 @@ var feed = new Instafeed({
   limit: 20,
   sortBy: 'most-liked',
   resolution: "standard_resolution",
-  accessToken: '2324131028.422b4d6.d6d71d31431a4e8fbf6cb1efa1a2dfdc',
+  accessToken: '',
   template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
 });
 feed.run();
